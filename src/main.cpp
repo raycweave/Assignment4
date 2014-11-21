d39514382418fbf14d7359baacaf5f45e52162d6
Ray Weaver <raycweave@gmail.com>
2014-11-20 23:18:11 -0700

    back to the laptop

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@
 #include <cstdlib>
 #include <cstdio>
 
+#include "testing.h"
 #include "Visit.h"
 
 using namespace std;
@@ -32,80 +33,13 @@ using namespace std;
 int main(int argc, char *argv [])
 {
 
-	Visit testData1;
-	Visit testData2;
-
-	Visit *testTemp1 = NULL;
-	Visit *testTemp2 = NULL;
-
-	testTemp1 = &testData1;
-	testTemp2 = &testData2;
 
 
 	cout << "Beginning tests." << endl;
 
-
-	testData1.setStartTime(100);
-	testData1.setEndTime(105);
-	testData1.setLocation("ECE Headquarters");
-
-	testData2.setStartTime(106.00084);
-	testData2.setEndTime(109.00025);
-	testData2.setLocation("Fourth Ave");
-
-
-	//////////////////////////////////////////////////////////////////
-	///////////////////Testing Visit()////////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-
-	if (testData1.getStartTime() != 100 && testData1.getEndTime() != 105 && testData1.getLocation() != "ECE Headquarters") 
-	{
-		cout << "   UNIT TEST FAILED: Visit()" << endl;						// need to write in the function name?
-	}
-
-	else 
-	{
-		cout << "   UNIT TEST PASSED: Visit()" << endl;
-	}
-
-
-	//////////////////////////////////////////////////////////////////
-	///////////////////Testing compare()//////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-
-	if (testData1.compare(testTemp1, testTemp2) == true)
-	{
-		cout << "   UNIT TEST PASSED: compare()" << endl;
-	}
-	
-	else 
-	{
-		cout << "   UNIT TEST FAILED: compare()" << endl;
-	}
-
-
-	//////////////////////////////////////////////////////////////////
-	///////////////////Testing addState()/////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-	testData1.addState("Init");
-	if (testData1.getStates().back() != "Init") 
-	{
-		cout << "   UNIT TEST FAILED: addState()" << endl;
-	}
-
-	else 
-	{
-		cout << "   UNIT TEST PASSED: addState()" << endl;
-	}
-
-	//////////////////////////////////////////////////////////////////
-	///////////////////Testing getVisitString()/////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-
+	testNonDefaultConstructor();
+	//testCompare();
+	testAddState();
 
 
 
--- src/testing.cpp
+++ src/testing.cpp
@@ -13,6 +13,7 @@
 #include "Visit.h"
 #include "testing.h"
 
+#include <string.h>
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
@@ -22,8 +23,11 @@ using namespace std;
 
 bool testNonDefaultConstructor() {
 	
+	bool result = false;
+
 	Visit testData1;
 	Visit testData2;
+	Visit testData3("Eegee's", 85805.037, 85808.557);
 
 	Visit *testTemp1 = NULL;
 	Visit *testTemp2 = NULL;
@@ -31,11 +35,15 @@ bool testNonDefaultConstructor() {
 	testTemp1 = &testData1;
 	testTemp2 = &testData2;
 
-	bool result = false;
-
 	testData1.setStartTime(100);
 	testData1.setEndTime(105);
 	testData1.setLocation("ECE Headquarters");
+
+	testData2.setStartTime(85801.037);
+	testData2.setEndTime(85802.037);
+	testData2.setLocation("ECE Back Lot");
+
+	
 
 	if (testData1.getStartTime() != 100 && testData1.getEndTime() != 105 && testData1.getLocation() != "ECE Headquarters")
 	{
@@ -46,19 +54,39 @@ bool testNonDefaultConstructor() {
 		result = false;
 	}
 
+	if (testData2.getStartTime() != 85801.037 && testData1.getEndTime() != 85802.037 && testData1.getLocation() != "ECE Back Lot")
+	{
+		result = false;
+	}
+
+	if (testData2.getStates().size() != 0) {
+		result = false;
+	}
 	
+	if (testData3.getStartTime() != 85805.037 && testData3.getEndTime() != 85808.557 && testData3.getLocation() != "Eegee's")
+	{
+		result = false;
+	}
+
+	if (testData3.getStates().size() != 0) {
+		result = false;
+	}
 
 	else
 	{
 		result = true;
 	}
+
 	return result;
 }
 
 bool testCompare() {
 
+	bool result = false;
+
 	Visit testData1;
 	Visit testData2;
+	Visit testData3("Eegee's", 85805.037, 85808.557);
 
 	Visit *testTemp1 = NULL;
 	Visit *testTemp2 = NULL;
@@ -66,17 +94,14 @@ bool testCompare() {
 	testTemp1 = &testData1;
 	testTemp2 = &testData2;
 
-	testData1.setLocation("Init");
-	testData2.setLocation("Ready");
+	testData1.setLocation("ECE HeadQuarters");
+	testData2.setLocation("John's House");
 
-	bool result = false;
+	if (Visit::compare(testTemp1,testTemp2) == false && testData1.getLocation().compare(testData2.getLocation()) > 0) {
 
-	if (str
-
-	else
-	{
-		result = false;
 	}
+	
+
 	return result;
 }
 
@@ -94,6 +119,10 @@ bool testAddState() {
 
 	if (testData1.getStates().back() != "Go")
 	{
+		result = false;
+	}
+
+	if (testData1.getStates().size() != 2) {
 		result = false;
 	}
 
